3d9d8130acc7ede4f5f1c7019a5b51593a01ed6d
Luis Eduardo Falcao <eduardo.cfalcao@gmail.com>
2023-06-29 19:07:25 -0300

    Remove folder cmd/server.

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "github.com/eduardocfalcao/vendors_checker/cmd/server"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/eduardocfalcao/vendors_checker/handlers"
+)
+
+const (
+	port = 8000
+)
 
 func main() {
-	server.Start()
+	address := fmt.Sprintf(":%d", port)
+	mux := http.NewServeMux()
+
+	registerRoutes(mux)
+
+	server := &http.Server{
+		Addr:         address,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	server.ListenAndServe()
+}
+
+func registerRoutes(mux *http.ServeMux) {
+	httpClient := &http.Client{Timeout: 1 * time.Second}
+
+	mux.HandleFunc("/v1/amazon-status", handlers.HandlerGetAmazonStatus(httpClient))
+	mux.HandleFunc("/v1/google-status", handlers.HandlerGetGoogleStatus(httpClient))
+	mux.HandleFunc("/v1/all-status", handlers.HandlerGetAllStatus(httpClient))
 }
