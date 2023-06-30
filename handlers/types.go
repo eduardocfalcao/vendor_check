72d6aa7aa39982006c734274cba24a274c424700
Luis Eduardo Falcao <eduardo.cfalcao@gmail.com>
2023-06-29 21:14:00 -0300

    Fix check vendor response status code field

--- handlers/types.go
+++ handlers/types.go
@@ -2,7 +2,7 @@ package handlers
 
 type CheckVendorResponse struct {
 	Url        string `json:"url"`
-	StatusCode int    `json:"statusCode"`
+	StatusCode int    `json:"status_code"`
 	Duration   int64  `json:"duration"`
 	Date       int64  `json:"date"`
 }
